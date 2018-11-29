6a6b98e98a0536c6c60ba1dfcd93addf5c7bb9af
Sergey Shelomentsev <sergey.v.shelomentsev@gmail.com>
2018-11-29 23:38:42 +0700

    Minor

--- database/init-database.js
+++ database/init-database.js
@@ -6,7 +6,7 @@ db._create("user");
 db._create("currency");
 db.currency.save({code: 'BTC', name: 'Bitcoin'});
 db.currency.save({code: 'XRP', name: 'Ripple'});
-db.currency.save({code: 'ETH', name: 'Etherium'});
+db.currency.save({code: 'ETH', name: 'Ethereum'});
 db.currency.save({code: 'DASH', name: 'Dash'});
 db.currency.save({code: 'LTC', name: 'Litecoin'});
 
