31d337e601184eb14582b91ad0ae5d72406cce3f
Sergey Shelomentsev <sergey.v.shelomentsev@gmail.com>
2018-12-01 04:43:41 +0700

    upd init scripts

--- database/init-database.js
+++ database/init-database.js
@@ -3,15 +3,12 @@ db._useDatabase("investment");
 
 db._create("user");
 
-db._create("cryptoCurrency");
-db.cryptoCurrency.save({code: 'BTC', name: 'Bitcoin'});
-db.cryptoCurrency.save({code: 'XRP', name: 'Ripple'});
-db.cryptoCurrency.save({code: 'ETH', name: 'Ethereum'});
-db.cryptoCurrency.save({code: 'DASH', name: 'Dash'});
-db.cryptoCurrency.save({code: 'LTC', name: 'Litecoin'});
-
-db._create("fiatCurrency");
-db.fiatCurrency.save({code: 'USD', name: 'United States Dollar'})
+db._create("currency");
+db.currency.save({code: 'BTC', name: 'Bitcoin'});
+db.currency.save({code: 'XRP', name: 'Ripple'});
+db.currency.save({code: 'ETH', name: 'Ethereum'});
+db.currency.save({code: 'DASH', name: 'Dash'});
+db.currency.save({code: 'LTC', name: 'Litecoin'});
+db.currency.save({code: 'USD', name: 'United States Dollar', type: 'fiat'});
 
 db._create("transaction");
-db._create("balance");
--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 
 mvn clean package -f backend/pom.xml
 
+#cd webapp
+#npm install
+#ng build --prod
+#cd ..
+
 docker-compose up --build --force-recreate
